4deacca9e0f1b4f99dab67838d8d17ac0c8b88a0
Phil Belinsky <philbelinsky@yahoo.com>
2025-12-30 15:38:54 -0500

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import {ENV} from './config/env'
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({origin: ENV.FRONTEND_URL}));
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments",
+        }
+    })
+})
+
+app.listen(ENV.PORT, ()=> console.log('Server is up and running on PORT:', ENV.PORT))
