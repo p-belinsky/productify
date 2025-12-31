e527f1518ffe3fb7d2e66101c6d20b2e8437f2d2
Phil Belinsky <philbelinsky@yahoo.com>
2025-12-30 19:11:06 -0500

    Add validation for DATABASE_URL before using non-null assertion.Refactor updateAt field in users and products table to include onUpdate behavior. Fix type inference for insert types - use  instead of .

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -8,7 +8,7 @@ export const users = pgTable("users", {
     name: text("name"),
     imageUrl: text("image_url"),
     createdAt: timestamp("created_at", {mode: "date"}).notNull().defaultNow(),
-    updatedAt: timestamp("updated_at", {mode: "date"}).notNull().defaultNow(),
+    updatedAt: timestamp("updated_at", {mode: "date"}).notNull().defaultNow().$onUpdate(()=> new Date()),
 })
 
 export const products = pgTable("products", {
@@ -20,7 +20,7 @@ export const products = pgTable("products", {
         .notNull()
         .references(()=> users.id, {onDelete: "cascade"}),
     createdAt: timestamp("created_at", {mode: "date"}).notNull().defaultNow(),
-    updatedAt: timestamp("updated_at", {mode: "date"}).notNull().defaultNow(),
+    updatedAt: timestamp("updated_at", {mode: "date"}).notNull().defaultNow().$onUpdate(()=> new Date()),
 
 })
 
@@ -54,10 +54,10 @@ export const commentRelations = relations(comments, ({one}) => ({
 }))
 
 export type User = typeof users.$inferSelect;
-export type NewUser = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
 
 export type Product = typeof products.$inferSelect;
-export type NewProduct = typeof products.$inferSelect;
+export type NewProduct = typeof products.$inferInsert;
 
 export type Comment = typeof comments.$inferSelect;
-export type NewComment = typeof comments.$inferSelect;
+export type NewComment = typeof comments.$inferInsert;
