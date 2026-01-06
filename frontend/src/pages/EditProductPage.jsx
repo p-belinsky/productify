131cc98490576038296a93f5b28d9c22b70bb2ad
Phil Belinsky <philbelinsky@yahoo.com>
2026-01-06 13:54:53 -0500

    feat: implement EditProduct functionality with form and update logic

--- frontend/src/pages/EditProductPage.jsx
+++ frontend/src/pages/EditProductPage.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import {useParams, useNavigate, Link} from "react-router";
+import { useAuth } from '@clerk/clerk-react';
+import {useProduct, useUpdateProduct} from "../hooks/useProducts.js";
+import LoadingSpinner from "../components/LoadingSpinner.jsx";
+import EditProductForm from "../components/EditProductForm.jsx";
+
 
 function EditProductPage() {
-    return (
-        <div>EditProductPage</div>
-    )
+
+    const {id} = useParams();
+    const {userId} = useAuth();
+    const navigate = useNavigate();
+
+    const {data: product, isLoading} = useProduct(id);
+    const updateProduct = useUpdateProduct();
+
+    if(isLoading) return <LoadingSpinner />;
+
+    if(!product || product.userId !== userId){
+        return (
+            <div className='card bg-base-300 max-w-md mx-auto'>
+                <div className='card-body items-center text-center'>
+                    <h2 className='card-title text-error'>{!product ? "Not found" : "Access denied"}</h2>
+                    <Link to="/" className='btn btn-primary btn-sm'>
+                        Go Home
+                    </Link>
+                </div>
+            </div>
+        )
+    }
+
+    return <EditProductForm product={product} isPending={updateProduct.isPending} isError={updateProduct.isError} onSubmit={(formData) => {
+        updateProduct.mutate({id, productData: formData}, {
+            onSuccess: () => navigate(`/product/${id}`),
+        });
+    }}/>
 }
 
 export default EditProductPage
